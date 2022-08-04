c5f44ae617b1380581448297a072af50952198af
Daemontrit <slavikk1999@yandex.ru>
2022-08-04 20:04:25 +0300

    bonus miles calculation

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice=2333;
+        int milePrice=20;
+        int bonusMile=ticketPrice/milePrice;
+        System.out.println("Бонусных миль: " + bonusMile);
+    }
+}
